11c32a654127423d34ffa7cb698f20ae0b3bfe22
blc <30732216+blcoyote@users.noreply.github.com>
2018-07-25 13:31:18 +0200

    .

---
+++ ConstantsC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConstantsC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers seperated by enter");
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            Console.WriteLine(input+input2);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpmasterclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
