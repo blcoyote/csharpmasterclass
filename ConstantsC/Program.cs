2f2b71dbabdc2aa9c0955bd79b5df5ddc56737fc
blc <30732216+blcoyote@users.noreply.github.com>
2018-08-17 13:11:25 +0200

    .

--- ConstantsC/Program.cs
+++ ConstantsC/Program.cs
@@ -9,7 +9,7 @@ namespace ConstantsC
             Console.WriteLine("enter two numbers seperated by enter");
             string input = Console.ReadLine();
             string input2 = Console.ReadLine();
-            Console.WriteLine(input+input2);
+            Console.WriteLine(Int32.Parse(input)+Int32.Parse(input2));
             Console.Read();
         }
 
